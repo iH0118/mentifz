d0afe0d70a937d5dbd56e79f93e0f0b5d338d917
iH0118 <iH0118@users.noreply.github.com>
2022-10-23 18:57:17 +0200

    strzerbuimhrzvbu

--- mentifz-wordcloud.sh
+++ mentifz-wordcloud.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
+# mentifz-wordcloud.sh
 # Author: iH0118
 # this script is officially gay
 
-while getopts 'h:' OPTION; do
+while getopts 'h' OPTION; do
   case "$OPTION" in
     h)
-      echo "you have supplied the -
-            h option"
+      echo "mentifz-wordcloud.sh help:
+
+            Basic Usage: mentifz-wordcloud.sh [-]
+            "
       ;;
     ?)
-      echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
-      exit 1
+      echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" #>&2
+#      exit 1
+      return 1
       ;;
   esac
 done
 shift "$(($OPTIND -1))"
+
+# get votekey from vote-id
+getvotekey(){
+  echo `curl -m 30 https://www.menti.com/core/vote-ids/$1/series | grep -o 'vote_key":"[0-9a-z]*' | sed 's/vote_key":"//'`
+}
+
+# get public key from votekey
+getpubkey() {
+  echo `curl -m 30 https://www.menti.com/core/vote-keys/$1/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//'`
+#  echo `curl -m 30 https://www.menti.com/core/vote-ids/$1/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//'`
+}
+
+
+
+#echo 'test'
