2be4c9b36399145e097a9646dbf59fa185be4030
iH0118 <iH0118@users.noreply.github.com>
2022-10-26 21:44:58 +0200

    cvwdasrbntbu,ni

--- mentifz-wordcloud.sh
+++ mentifz-wordcloud.sh
@@ -6,26 +6,28 @@
 
 ## THIS IS VERY INCOMPLETE
 
-VOTEID=
+
 VOTEKEY=
 COUNT=
 PUBKEY=
 
-while getopts 'hk:' OPTION; do
+while getopts 'hk:c:' OPTION; do
   case "$OPTION" in
     h)
       echo "Usage: mentifz-wordcloud.sh [options] <input data>
             -k  vote key / vote id
-            -c  number of 
+            -c  number of requests
             "
       ;;
     k)
-      
-      ;;
+      local KEY="$OPTARG"
 
-    
+      ;;
+    c(
+
+      ;;
     ?)
-      echo "Usage: mentifz-wordcloud.sh [options] <input data>" #>&2
+      echo "Usage: mentifz-wordcloud.sh [options] <input data>"
 #      exit 1
       return 1
       ;;
@@ -37,18 +39,18 @@ shift "$OPTIND"
 
 # get votekey from vote-id
 getvotekey(){
-  echo `curl -m 30 https://www.menti.com/core/vote-ids/$1/series | grep -o 'vote_key":"[0-9a-z]*' | sed 's/vote_key":"//'`
+  echo $(curl -m 30 https://www.menti.com/core/vote-ids/${1}/series | grep -o 'vote_key":"[0-9a-z]*' | sed 's/vote_key":"//')
 }
 
 # get public key from votekey
 getpubkey() {
-  echo `curl -m 30 https://www.menti.com/core/vote-keys/$1/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//'`
+  echo $(curl -m 30 https://www.menti.com/core/vote-keys/${1}/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//')
 }
 
-# get public key from vote-id
-getpubkey_id() {
-  echo `curl -m 30 https://www.menti.com/core/vote-ids/$1/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//'`
-}
+## get public key from vote-id
+#getpubkey_id() {
+#  echo $(curl -m 30 https://www.menti.com/core/vote-ids/${1}/series | grep -o 'public_key":"[0-9a-f]*' | sed 's/public_key":"//')
+#}
 
 
 
